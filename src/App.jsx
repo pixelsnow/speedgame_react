19ada71a72067b243eefd97d7ec5aeafafdcd25c
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-11-16 15:05:54 +0200

    add sound functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import "./App.css";
 import { Component } from "react";
 import Circle from "./Circle";
 import Modal from "./Modal";
-/* import click from "./assets/sounds/click.wav";
+import Life from "./Life";
+import click from "./assets/sounds/click1.mp3";
+import crack0 from "./assets/sounds/crack5.mp3";
+import crack1 from "./assets/sounds/popcorn1.mp3";
+import crack2 from "./assets/sounds/crack4.mp3";
+import crack3 from "./assets/sounds/slime_cut.mp3";
+import spook from "./assets/sounds/spook2.mp3";
 
-let clickSound = new Audio(click); */
+let clickSound = new Audio(click);
+let crackSound;
+let crackSound0 = new Audio(crack0);
+let crackSound1 = new Audio(crack1);
+let crackSound2 = new Audio(crack2);
+let crackSound3 = new Audio(crack3);
+let gameOver = new Audio(spook);
 
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -27,6 +39,13 @@ class App extends Component {
 
   timer;
 
+  // Initialises lives array to map later in the render
+  initLivesArray = () => {
+    let lives = [];
+    for (let i = 0; i < this.state.lives; i++) lives.push(i);
+    this.setState({ livesArray: lives });
+  };
+
   closeModal = () => {
     this.setState({ modalActive: false });
     this.resetGame();
@@ -43,45 +62,67 @@ class App extends Component {
   };
 
   endGame = () => {
+    if (!crackSound.paused) crackSound.pause();
+    if (!clickSound.paused) clickSound.pause();
+    gameOver.play();
     this.toggleButtons();
-    this.setState({ modalActive: true, lives: 0 });
+    this.setState({ modalActive: true });
     clearTimeout(this.timer);
   };
 
   resetGame = () => {
-    this.setState({
-      current: undefined,
-      lives: 3,
-      score: 0,
-      pace: 1000,
-      correctClicked: true,
-    });
+    window.location.reload();
   };
 
+  // On page load, init circles array and lives array
   componentDidMount() {
     let res = [];
     for (let i = 0; i < this.state.circlesNum; i++) res.push(i);
     this.setState({ circles: res });
-    let lives = [];
-    for (let i = 0; i < this.state.lives; i++) lives.push(i);
-    this.setState({ livesArray: lives });
+    this.initLivesArray();
   }
 
-  /* clickPlay = () => {
+  clickPlay = () => {
     if (clickSound.paused) {
       clickSound.play();
     } else {
       clickSound.currentTime = 0;
     }
-  }; */
+  };
 
   clickCircle = (key) => {
-    // If game is not in progress, don't to anything
+    switch (key) {
+      case 0: {
+        crackSound = crackSound0;
+        break;
+      }
+      case 1: {
+        crackSound = crackSound1;
+        break;
+      }
+      case 2: {
+        crackSound = crackSound2;
+        break;
+      }
+      case 3: {
+        crackSound = crackSound3;
+        break;
+      }
+      default:
+    }
+    if (crackSound.paused) {
+      crackSound.play();
+    } else {
+      crackSound.currentTime = 0;
+    }
+    // If game is not in progress, don't do anything
     if (!this.state.gameInProgress) return;
-    /* this.clickPlay(); */
+    this.clickPlay();
+    // If the wrong circle was clicked, end the game
     if (key !== this.state.current) {
       this.endGame();
     } else {
+      // if the correct circle was clicked, increase the score and toggle the correctClicked flag
       this.setState({
         score: this.state.score + 1,
         correctClicked: true,
@@ -90,9 +131,13 @@ class App extends Component {
   };
 
   nextCircle = () => {
+    console.log(`pace: ${this.state.pace}`);
     // Checking if the correct circle wasn't clicked and we should reduce lives
     if (!this.state.correctClicked) {
-      this.setState({ lives: this.state.lives - 1 });
+      this.setState({
+        lives: this.state.lives - 1,
+        livesArray: this.state.livesArray.slice(1),
+      });
       if (this.state.lives <= 1) {
         this.endGame();
         return;
@@ -128,12 +173,14 @@ class App extends Component {
       />
     ));
 
+    const lives = this.state.livesArray.map((life, i) => <Life key={i} />);
+
     return (
       <div className="App">
         <h1>SPEEDGAME</h1>
         <div className="game-info-bar">
           <p className="game-score">score: {this.state.score}</p>
-          <p className="lives">♥ {this.state.lives}</p>
+          <p className="lives">{lives}</p>
         </div>
 
         <div className="circle-container">{circles}</div>
