255696cbfc6a579fde9d17fdb622144abb50a8f0
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-11-16 10:37:02 +0200

    add working version with console.logs

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,15 @@ class App extends Component {
   state = {
     score: 0,
     lives: 3,
+    livesArray: [],
     circlesNum: 4,
     circles: [],
     startButtonActive: true,
-    endButtonActive: false,
+    gameInProgress: false,
     modalActive: false,
     current: undefined,
     pace: 1000,
-    correctClicked: false,
+    correctClicked: true,
   };
 
   timer;
@@ -33,7 +34,7 @@ class App extends Component {
 
   toggleButtons = () => {
     this.setState({ startButtonActive: !this.state.startButtonActive });
-    this.setState({ endButtonActive: !this.state.endButtonActive });
+    this.setState({ gameInProgress: !this.state.gameInProgress });
   };
 
   startGame = () => {
@@ -43,19 +44,27 @@ class App extends Component {
 
   endGame = () => {
     this.toggleButtons();
-    this.setState({ modalActive: true });
+    this.setState({ modalActive: true, lives: 0 });
     clearTimeout(this.timer);
   };
 
   resetGame = () => {
-    this.setState({ current: undefined, lives: 3, score: 0, pace: 1000 });
+    this.setState({
+      current: undefined,
+      lives: 3,
+      score: 0,
+      pace: 1000,
+      correctClicked: true,
+    });
   };
 
   componentDidMount() {
     let res = [];
     for (let i = 0; i < this.state.circlesNum; i++) res.push(i);
-    console.log(`circles array set to: ${res}`);
     this.setState({ circles: res });
+    let lives = [];
+    for (let i = 0; i < this.state.lives; i++) lives.push(i);
+    this.setState({ livesArray: lives });
   }
 
   /* clickPlay = () => {
@@ -67,37 +76,59 @@ class App extends Component {
   }; */
 
   clickCircle = (key) => {
-    if (!this.state.endButtonActive) return;
+    // If game is not in progress, don't to anything
+    if (!this.state.gameInProgress) return;
     /* this.clickPlay(); */
-    console.log("circle ", key, " clicked");
     if (key !== this.state.current) {
-      this.setState({ lives: this.state.lives - 1 });
+      this.endGame();
     } else {
       this.setState({
         score: this.state.score + 1,
         correctClicked: true,
-        /* lives: this.state.lives + 1, */
       });
     }
   };
 
   nextCircle = () => {
-    if (!this.state.lives) {
-      this.endGame();
-      return;
+    // Checking if the correct circle wasn't clicked and we should reduce lives
+    if (!this.state.correctClicked) {
+      console.log(
+        `not clicked correct, reducing lives to ${this.state.lives - 1}`
+      );
+      this.setState({ lives: this.state.lives - 1 });
+      if (this.state.lives <= 1) {
+        this.endGame();
+        return;
+      }
     }
+    // Get the next circle
     let nextActive;
     do {
       nextActive = getRandomInt(0, this.state.circlesNum - 1);
     } while (nextActive === this.state.current);
+    //
     this.setState({
       current: nextActive,
       pace: this.state.pace - 20,
-      // TODO: handle lives with clickedCorrect state
-      lives: this.state.lives - 1,
     });
-    console.log(nextActive);
-    this.timer = setTimeout(this.nextCircle, this.state.pace);
+    if (!this.state.correctClicked) {
+      console.log(
+        `not clicked correct, reducing lives to ${this.state.lives - 1}`
+      );
+      this.setState({ lives: this.state.lives - 1 });
+      if (this.state.lives <= 1) {
+        this.endGame();
+        return;
+      }
+    }
+    console.log(`current: ${nextActive}`);
+    this.setState({ correctClicked: false });
+    console.log("correctClicked set to false");
+    this.timer = setTimeout(
+      this.nextCircle,
+      this.state.pace,
+      this.state.correctClicked
+    );
   };
 
   render() {
@@ -115,7 +146,7 @@ class App extends Component {
         <h1>SPEEDGAME</h1>
         <div className="game-info-bar">
           <p className="game-score">score: {this.state.score}</p>
-          <p className="lives">♥ ♥ ♥{this.state.lives}</p>
+          <p className="lives">♥{this.state.lives}</p>
         </div>
 
         <div className="circle-container">{circles}</div>
@@ -124,7 +155,7 @@ class App extends Component {
             start playing
           </button>
         )}
-        {this.state.endButtonActive && (
+        {this.state.gameInProgress && (
           <button className="end-btn" onClick={this.endGame}>
             stop playing
           </button>
