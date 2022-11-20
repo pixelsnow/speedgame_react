35a6e464314f46116938b3c982dcf02e2f83af61
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-11-20 18:40:18 +0200

    add screenshot

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = (props) => {
     <div className="overlay">
       <div className="modal">
         <h2>GAME OVER</h2>
-        <p className="final-score">score: {props.score}</p>
+        <p className="final-score">Got {props.score}</p>
         <p className="game-over-message">{props.message}</p>
         <p className="game-over-submessage">{props.submessage}</p>
         <button className="close-modal" onClick={props.closeModal}>
